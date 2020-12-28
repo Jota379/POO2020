109b6f383b1167510c4a0479b0d6c91f643a6b31
Pinacolada <57179508+PinaDesu@users.noreply.github.com>
2020-12-28 19:12:23 +0000

    commit

--- TrabPra/Interacao.h
+++ TrabPra/Interacao.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include "Mundo_Territorio.h"
+using namespace std;
+
+
+class Interacao {
+private:
+
+
+public:
+	int comandline();
+	void mostra_menu();
+	void escrita_menus(vector<Mundo_Territorios*> jogo);
+
+};
